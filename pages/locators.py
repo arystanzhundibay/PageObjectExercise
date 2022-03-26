1f9841de0c1d7f0dfc3c0fdb3272eba99ee39c9f
arystanzhundibay <arystan.zhundibay@gmail.com>
2022-03-26 19:30:14 +0500

    update main page and tests, add locators, add login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.ID, "login_link")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.CSS_SELECTOR, 'input[name=\'login-username\']')
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, 'input[name=\'login-password\']')
+    REGISTER_EMAIL = (By.CSS_SELECTOR, 'input[name=\'registration-email\']')
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, 'input[name=\'registration-password1\']')
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, 'input[name=\'registration-password2\']')
--- test_main_page.py
+++ test_main_page.py
@@ -11,3 +11,16 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+from .pages.login_page import LoginPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
