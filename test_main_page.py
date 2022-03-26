a132cca1a25017ca5f15adfefe759ab0232d1f35
arystanzhundibay <arystan.zhundibay@gmail.com>
2022-03-26 16:17:24 +0500

    add base page object, update test file

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,13 @@
+from .pages.main_page import MainPage
+from selenium.webdriver.common.by import By
+
+
+# def should_be_login_link(self):
+#     assert self.is_element_present(By.ID, "login_link"), "Login link is not presented"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
-
-
-# from .pages.main_page import MainPage
-#
-#
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
